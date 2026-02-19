e5d51af40c46bbdd7611e4b30d9a8e6096eae7c5
lucasquirino2912-source <lucasquirino2912@gmail.com>
2026-02-18 23:35:24 -0300

    fix: add minimal puppeteer sandbox args for Docker root execution

--- robo.js
+++ robo.js
@@ -7,6 +7,14 @@ const { Client, MessageMedia, LocalAuth } = require("whatsapp-web.js");
 // =====================================
 const client = new Client({
   authStrategy: new LocalAuth(),
+  puppeteer: {
+    headless: true,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+    ],
+  },
 });
 
 // =====================================
